291ff53f82daa9990fa6b767bc87e57a995e2b9b
LadyKerr <kadiekerr@gmail.com>
2019-04-23 19:32:57 -0400

    started working on prototypes

--- assignments/prototypes.js
+++ assignments/prototypes.js
@@ -1,28 +1,39 @@
 /*
-  Object oriented design is commonly used in video games.  For this part of the assignment you will be implementing several constructor functions with their correct inheritance hierarchy.
+  Object oriented design is commonly used in video games.  
+  For this part of the assignment you will be implementing several constructor functions with their correct inheritance hierarchy.
 
   In this file you will be creating three constructor functions: GameObject, CharacterStats, Humanoid.  
 
-  At the bottom of this file are 3 objects that all end up inheriting from Humanoid.  Use the objects at the bottom of the page to test your constructor functions.
+  At the bottom of this file are 3 objects that all end up inheriting from Humanoid.  
+  Use the objects at the bottom of the page to test your constructor functions.
   
   Each constructor function has unique properties and methods that are defined in their block comments below:
 */
   
-/*
-  === GameObject ===
-  * createdAt
-  * name
-  * dimensions (These represent the character's size in the video game)
-  * destroy() // prototype method that returns: `${this.name} was removed from the game.`
-*/
 
-/*
-  === CharacterStats ===
-  * healthPoints
-  * takeDamage() // prototype method -> returns the string '<object name> took damage.'
-  * should inherit destroy() from GameObject's prototype
-*/
+  // === GameObject ===
+  Function GameObject(attrs) {
+    this.createdAt = attrs.createdAt;
+    this.name = attrs.name; 
+    this.dimensions = attrs.dimensions; //(These represent the character's size in the video game)
+  };
+   // prototype method that returns: `${this.name} was removed from the game.`
+  GameObject.prototype.destroy = function() {
+    return `${this.name} was removed from the game.`;
+  };
 
+
+ // === CharacterStats ===
+ Function CharacterStats(StatsAttrs) {
+   GameObjects.call(this, CharacterStats);
+  this.healthPoints = StatsAttrs.healthPoints;
+ };
+
+ CharacterStats.prototype = Object.create(GameObject.prototype); //giving full inheritance of parent GameObject; //should inherit destroy() from GameObject's prototype
+ CharacterStats.prototype.takeDamage = function() {
+   return `${this.name} took damage`; // prototype method -> returns the string '<object name> took damage.'
+ }; 
+  
 /*
   === Humanoid (Having an appearance or character resembling that of a human.) ===
   * team
@@ -106,5 +117,6 @@
 
   // Stretch task: 
   // * Create Villain and Hero constructor functions that inherit from the Humanoid constructor function.  
-  // * Give the Hero and Villains different methods that could be used to remove health points from objects which could result in destruction if health gets to 0 or drops below 0;
+  // * Give the Hero and Villains different methods that could be used to remove health points from objects which could result 
+  // in destruction if health gets to 0 or drops below 0;
   // * Create two new objects, one a villain and one a hero and fight it out with methods!
